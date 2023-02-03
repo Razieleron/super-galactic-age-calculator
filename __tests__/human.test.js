97f96d0959be4803e743ace07666e0350fe223bd
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-02-03 13:46:30 -0800

    RGR test for human.test.js: Good Fail!

---
+++ __tests__/human.test.js
@@ -0,0 +1,9 @@
+import Human from './../src/js/human.js';
+describe('Human', () => {
+
+  test('should correctly create a human object with a name and an earth age', () => {
+    const human = new Human("Dan Dimmerdome", 27);
+    expect(human.name).toEqual("Chuck Norris");
+
+  });
+});
---
+++ src/js/human.js
@@ -0,0 +1,19 @@
+export default class Human {
+  constructor(name, currentAge) {
+    this.name = name;
+    this.currentAge = currentAge
+  }
+}
+
+//   checkType() {
+//     if ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) {
+//       return "not a triangle";
+//     } else if ((this.side1 !== this.side2) && ((this.side1 !== this.side3)) && ((this.side2 !== this.side3))) {
+//       return "scalene triangle";
+//     }  else if ((this.side1 === this.side2) && (this.side1 === this.side3)) {
+//       return "equilateral triangle";
+//     } else {
+//       return "isosceles triangle";
+//     }
+//   }    
+// }
